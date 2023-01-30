4b0446821bb584c0703a7fd88e57e7253e1b5dbe
Kevon Adams <Kevon.Adams@ibm.com>
2023-01-30 16:13:42 -0600

    added onChange to editToggle component

--- src/components/EditToggle.js
+++ src/components/EditToggle.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class EditToggle extends Component {
     render() {
         return (
-            <select className="ToggleSwitch">
+            <select className="ToggleSwitch" onChange={ (e) => this.props.update(e.target.value)}>
                 <option value="true"> On </option>
                 <option value="false"> Off </option>
             </select>
